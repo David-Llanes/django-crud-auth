24785cc4e6e4fb6aeb0d47cb0129216517595515
David-Llanes <david.llanes1019@gmail.com>
2023-03-14 18:57:27 -0700

    commente pip install requirements.txt

--- build.sh
+++ build.sh
@@ -2,8 +2,9 @@
 # exit on error
 set -o errexit
 
+# No tenemos que ejecutar estos comandos ya que Render se encargará de hacerlo.
 #poetry install
-pip install -r requirements.txt
+#pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
