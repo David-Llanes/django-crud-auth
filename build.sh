5595e68e20fc6f52180b27340c0919b8c4847d77
David-Llanes <david.llanes1019@gmail.com>
2023-03-14 19:28:54 -0700

    build.sh test

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 set -o errexit
 
 # No tenemos que ejecutar estos comandos ya que Render se encargará de hacerlo.
-#poetry install
-#pip install -r requirements.txt
+# poetry install
+pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
